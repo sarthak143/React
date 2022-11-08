f7304405bf1a60edb6c139dbd38b52ce403f7636
sarthak bansal <39374452+sarthak143@users.noreply.github.com>
2022-11-08 22:38:23 +0530

    Revert "push notification"
    This reverts commit d60555c416662e0a60168dd507cb5b865ae2f04d.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -2,21 +2,10 @@
 import { Link } from "react-router-dom";
 import Right from "./Right";
 import Left from "./Left";
-import addNotification, { Notifications } from "react-push-notification";
-const buttonClick = () => {
-  console.log("1");
-  addNotification({
-    title: "Sarthak messaged you",
-    subtitle: "This is a subtitle",
-    message: "Click to Open this",
-    theme: "darkblue",
-    native: true, // when using native, your OS will handle theming.
-  });
-};
+
 function Homepage() {
   return (
     <div className="content">
-      <Notifications />
       {/* <!-- Start Content--> */}
       <div className="container-fluid">
         {/* <!-- start page title --> */}
@@ -25,13 +14,9 @@ function Homepage() {
             <div className="page-title-box">
               <div className="page-title-right">
                 <form className="d-flex">
-                  <button
-                    onClick={buttonClick}
-                    to="/"
-                    className="btn btn-primary ms-2"
-                  >
+                  <Link to="/" className="btn btn-primary ms-2">
                     <i className="mdi mdi-autorenew"></i>
-                  </button>
+                  </Link>
                 </form>
               </div>
               <h4 className="page-title">Dashboard</h4>
