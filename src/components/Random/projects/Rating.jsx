87444f8707290cb9dcd756a98337afc43f4b020e
sarthak bansal <39374452+sarthak143@users.noreply.github.com>
2022-11-26 01:16:54 +0530

    star

--- src/components/Random/projects/Rating.jsx
+++ src/components/Random/projects/Rating.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import "./css/rating.css";
 const Rating = () => {
-  let limit = 5;
   const [initialValue, setInitialValue] = React.useState(2);
   const handleClick = (event) => {
     setInitialValue(+event.target.getAttribute("data"));
   };
 
-  const handleDoubleClick = (event) => {
-    if (+event.target.getAttribute("data") === 1) {
-      setInitialValue(0);
-    }
-  };
   return (
-    <div onClick={handleClick} onDoubleClick={handleDoubleClick}>
-      {[...new Array(limit).keys()].map((param) => (
+    <div onClick={handleClick}>
+      {[...new Array(5).keys()].map((param) => (
         <span
           key={param}
           data={param + 1}
